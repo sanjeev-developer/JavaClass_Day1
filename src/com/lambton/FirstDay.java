36a3bacd73b5cd3495d05d4fc2556a9cafba3320
The Shimmering World <sanjeevgupta9223@gmail.com>
2019-09-18 16:16:01 -0700

    Initial commit

---
+++ src/com/lambton/FirstDay.java
@@ -0,0 +1,14 @@
+package com.lambton;
+
+public class FirstDay {
+
+    public static void runfirst()
+    {
+        System.out.println("Sanjeev first");
+    }
+    public static void main(String[] args) {
+        int a =4;
+        int b = 3;
+        System.out.println("output"+(a+b));
+    }
+}
